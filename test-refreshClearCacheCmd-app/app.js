0a1ba70085e54b4d5a2c49c34526806852f7e596
Luca Greco <luca.greco@alcacoop.it>
2013-06-17 23:32:32 +0200

    test-refreshClearCacheCmd-app: revert to click event

--- test-refreshClearCacheCmd-app/app.js
+++ test-refreshClearCacheCmd-app/app.js
@@ -1,43 +1,49 @@
 window.onload = function () {
   document.querySelector("#test-localStorage button.get").
-    addEventListener("touchstart", function localStorageGet() {
+    addEventListener("click", function localStorageGet(evt) {
       console.log("GET 'test-key' from localStorage");
       var value = localStorage.getItem("test-key");
       document.querySelector("#test-localStorage input").value = value;
+      evt.preventDefault();
     }, false);
 
   document.querySelector("#test-localStorage button.set").
-    addEventListener("touchstart", function localStorageSet() {
+    addEventListener("click", function localStorageSet(evt) {
       console.log("SET 'test-key' from localStorage");
       var value = document.querySelector("#test-localStorage input").value;
       localStorage.setItem("test-key", value || "test1");
+      evt.preventDefault();
     }, false);
 
   document.querySelector("#test-sessionStorage button.get").
-    addEventListener("touchstart", function sessionStorageGet() {
+    addEventListener("click", function sessionStorageGet(evt) {
       console.log("GET 'test-key' from sessionStorage");
       var value = sessionStorage.getItem("test-key");
       document.querySelector("#test-sessionStorage input").value = value;
+      evt.preventDefault();
     }, false);
 
   document.querySelector("#test-sessionStorage button.set").
-    addEventListener("touchstart", function sessionStorageSet() {
+    addEventListener("click", function sessionStorageSet(evt) {
       console.log("SET 'test-key' from sessionStorage");
       var value = document.querySelector("#test-sessionStorage input").value;
       sessionStorage.setItem("test-key",  value || "test2");
+      evt.preventDefault();
     }, false);
 
   document.querySelector("#test-cookies button.get").
-    addEventListener("touchstart", function cookiesGet() {
+    addEventListener("click", function cookiesGet(evt) {
       console.log("GET cookies");
       document.querySelector("#test-cookies input").value = document.cookie;
+      evt.preventDefault();
     }, false);
 
   document.querySelector("#test-cookies button.set").
-    addEventListener("touchstart", function cookiesSet() {
+    addEventListener("click", function cookiesSet(evt) {
       console.log("SET cookies");
       var value = document.querySelector("#test-cookies input").value;
       document.cookie = "test=" + value  || "test=value";
+      evt.preventDefault();
     }, false);
 
   var req = navigator.mozApps.getSelf();
